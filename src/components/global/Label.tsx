9d3b8014bd7a39e5cfb2b7debd65a608580ad29b
agnivon <agnivon@gmail.com>
2024-03-13 17:31:18 +0530

    Updates

--- src/components/global/Label.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import { Text } from "react-native";
-
-export default function Label(props: PropsWithChildren<{}>) {
-  return (
-    <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.children}</Text>
-  );
-}
